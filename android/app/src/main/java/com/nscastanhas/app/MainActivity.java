b49f8b6286264700d5787fc8aff8d3ac6aba6430
User <user@example.com>
2026-03-16 16:51:50 -0400

    Add Capacitor and GitHub Actions to build Android APK

---
+++ android/app/src/main/java/com/nscastanhas/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.nscastanhas.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nscastanhas.app',
+  appName: 'NS Castanhas',
+  webDir: 'dist'
+};
+
+export default config;
